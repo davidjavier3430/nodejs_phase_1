d5944b51604e581e79f0da750e96dae061676ca9
davidjavier3430 <davidjavier3430@gmail.com>
2016-07-26 23:03:56 -0400

    First commit module 1 exercise 1 & 2

---
+++ module_1/exercise_1.js
@@ -0,0 +1,21 @@
+console.log("Exercise #1");
+//Number
+console.log(1);
+
+//Variable
+var x = 1;
+console.log(x);
+
+//Expression
+console.log(x + 2);
+
+//Multi Arguments
+console.log(x, x, 1, 2 + 2);
+
+// + example #1
+console.log(1 + 1);
+
+// + example #2
+var a = 3;
+var b = 5;
+console.log(a + b);
---
+++ module_1/exercise_2.js
@@ -0,0 +1,10 @@
+console.log("Exercise #2");
+
+console.log("Post-Increment");
+var x = 3;
+console.log(x++);
+console.log(x);
+
+console.log("Pre-Increment");
+var y = 6;
+console.log(++y);
